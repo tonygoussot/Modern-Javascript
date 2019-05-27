0cdd227bee044f4c4600b2fc1e90324c65ca3679
Brice Marchand <brice.marchand91@gmail.com>
2019-05-27 18:45:30 +0200

    Add TypeScript Exercice

---
+++ exercises/typescript/exo.ts
@@ -0,0 +1,37 @@
+const add = (a, b) => a + b;
+const includeInListOfString = (list) => (item) => {
+    return list.indexOf(item) !== -1;
+};
+
+add(12, 100);
+includeInListOfString(['1', '3'])('3');
+includeInListOfString(['1', '3'])(3); // should Error
+
+
+// EVehicleName
+const includeInIllegalVehicle = includeInListOfString([EVehicleName.BMW]);
+
+
+// IVehicle
+const isIllegalVehicle = ({ name }) => includeInIllegalVehicle(name);
+
+isIllegalVehicle(?);
+
+
+// IVehicleOptions
+const getVehiclePricesOptions = (vehicleOptions) => {
+    if (vehicleOptions && vehicleOptions.length > 0) {
+        return vehicleOptions.reduce((acc, o) => add(acc, o.pricing.netPriceInclTax), 0);
+    }
+    return 0;
+};
+
+getVehiclePricesOptions(?);
+
+// Generic
+function combineData(first, second) {
+    return {...first, ...second};
+}
+
+// TFullVehicleInformation
+const getFullVehicleInformation = (vehicle, vehicleOptions) => combineData(vehicle, vehicleOptions);
