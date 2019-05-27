e3e71c4ee88ecbafe6f9e251cc1267d79309a6f7
Anthony GOUSSOT <anthonygoussot@mac365-1.home>
2019-05-27 08:15:23 +0200

    generators solution

--- exercises/generators/exo.js
+++ exercises/generators/exo.js
@@ -1,5 +1,11 @@
-const generator = function* {
+const generator = function* fibonacci() {
+    let current = 0;
+    let next = 1;
 
+    while (true) {
+        yield current;
+        [current, next] = [next, current + next]
+    }
 };
 
 module.exports = {generator};
